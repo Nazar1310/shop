1caf9314d79dd12e060d16df3499e959de2efb5a
nazar <nazar131019@gmail.com>
2022-07-27 11:48:36 +0300

    fix

--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -1,12 +1,12 @@
 <section class="banner spad">
     <div class="container">
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-md-6">
                 <div class="banner__item__text_v2">
                     <p>Smo nova znamka spodnjega perila. Slovi po kakovosti, mehkobi in izvirnih ročnih detajlih, ki poskrbijo za unikaten videz. Vsak kos je ustvarjen z ljubeznijo, nežnostjo in namenom poudarka vaše ženstvenosti.</p>
                 </div>
             </div>
-            <div class="col-lg-6">
+            <div class="col-md-6">
                 <div class="banner__item">
                     <div class="banner__item__pic">
                         <img src="/img/banner-1.jpg" alt="">
