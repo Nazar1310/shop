a4a07f3d012a89ced3ec20267d7e70d7d6669f91
nazar <nazar131019@gmail.com>
2022-07-27 11:41:19 +0300

    fix

--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -1,13 +1,15 @@
 <section class="banner spad">
     <div class="container">
         <div class="row">
-            <div class="col-lg-7 offset-lg-4">
+            <div class="col-lg-6">
+                <div class="banner__item__text_v2">
+                    <p>Smo nova znamka spodnjega perila. Slovi po kakovosti, mehkobi in izvirnih ročnih detajlih, ki poskrbijo za unikaten videz. Vsak kos je ustvarjen z ljubeznijo, nežnostjo in namenom poudarka vaše ženstvenosti.</p>
+                </div>
+            </div>
+            <div class="col-lg-6">
                 <div class="banner__item">
                     <div class="banner__item__pic">
                         <img src="/img/banner/banner-1.jpg" alt="">
-                    </div>
-                    <div class="banner__item__text">
-                        <p>Smo nova znamka spodnjega perila. Slovi po kakovosti, mehkobi in izvirnih ročnih detajlih, ki poskrbijo za unikaten videz. Vsak kos je ustvarjen z ljubeznijo, nežnostjo in namenom poudarka vaše ženstvenosti.</p>
                     </div>
                 </div>
             </div>
